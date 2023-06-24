1e6cb630c3bc7afc7d5578c3199c0ef3bb4bdecf
Ivan Shevchenko <kidscrew2017@gmail.com>
2023-06-24 18:27:42 +0300

    приложение project

--- portfolio/portfolio/views.py
+++ portfolio/portfolio/views.py
@@ -1,4 +1,14 @@
+# from django.contrib import admin
+# from django.urls import path, include
+# from . import views
+#
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('hello/', include('hello_world.urls'))
+# ]
+
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'home.html')
+def base(request):
+    return render(request, 'base.html')
+
